7ca3e2a733e1fd793ce71a050f9a1822b4cb6c7a
Kari Engen Guttulsrud <eparis@atlassian.com>
2016-01-24 16:43:25 +0100

    jkerg

---
+++ oving3TCP.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "os"
+)
+
+
+const port 		= "30000"
+const localIP 	= "129.241.187.143"
+const ServerIP  = "129.241.187.255"
+
+
+const (
+    CONN_HOST = localIP
+    CONN_PORT = port
+    CONN_TYPE = "tcp"
+)
+
+func server() {
+    // Listen for incoming connections.
+    l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+    if err != nil {
+        fmt.Println("Error listening:", err.Error())
+        os.Exit(1)
+    }
+    // Close the listener when the application closes.
+    defer l.Close()
+    fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+    for {
+        // Listen for an incoming connection.
+        conn, err := l.Accept()
+        if err != nil {
+            fmt.Println("Error accepting: ", err.Error())
+            os.Exit(1)
+        }
+        // Handle connections in a new goroutine.
+        go handleRequest(conn)
+    }
+}
+
+// Handles incoming requests.
+func handleRequest(conn net.Conn) {
+  // Make a buffer to hold incoming data.
+  buf := make([]byte, 1024)
+  // Read the incoming connection into the buffer.
+  _, err := conn.Read(buf)
+  if err != nil {
+    fmt.Println("Error reading:", err.Error())
+  }
+  // Send a response back to person contacting us.
+  conn.Write([]byte("Message received."))
+  // Close the connection when you're done with it.
+  conn.Close()
+}
+
+func client() {
+    strEcho := "Halo"
+    servAddr := CONN_HOST+":"+CONN_PORT
+    tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
+    if err != nil {
+        println("ResolveTCPAddr failed:", err.Error())
+        os.Exit(1)
+    }
+
+    conn, err := net.DialTCP("tcp", nil, tcpAddr)
+    if err != nil {
+        println("Dial failed:", err.Error())
+        os.Exit(1)
+    }
+
+    _, err = conn.Write([]byte(strEcho))
+    if err != nil {
+        println("Write to server failed:", err.Error())
+        os.Exit(1)
+    }
+
+    println("write to server = ", strEcho)
+
+    reply := make([]byte, 1024)
+
+    _, err = conn.Read(reply)
+    if err != nil {
+        println("Write to server failed:", err.Error())
+        os.Exit(1)
+    }
+
+    println("reply from server=", string(reply))
+
+    conn.Close()
+}
+
+func main(){
+	server()
+	//client()
+}
+
